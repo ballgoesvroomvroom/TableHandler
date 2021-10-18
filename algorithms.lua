b2846eb386f279ab3be62e73918f1e5997ee3cb2
FadedJayden <chongjayden07@gmail.com>
2021-10-18 23:16:58 +0800

    initial commit :)

---
+++ algorithms.lua
@@ -0,0 +1,142 @@
+algorithms = {}
+--[[
+Changes will be made to passed array (Modifies the passed array).
+Parser(SorterObj) would have made a copy array where number elements are filtered into it.
+All sorting functions assume that the array passed contains valid elements.
+All sorted occurs in ascending order.
+--]]
+
+algorithms.LENGTH = 4 -- amount of algorithms
+
+algorithms.dir = {
+	algorithms.bubblesort,
+	algorithms.insertionsort,
+	algorithms.heapsort,
+	algorithms.quicksort
+}
+function algorithms.distributor(algoNo: number, array, sortingargs)
+	if type(algoNo) ~= "number" or algoNo <= 0 or algoNo > #algorithms.dir then
+		error("Invalid argument passed, 'algoNo', must be a number between 1 and %d (inclusive)":format(#algorithms.dir))
+	else
+		if #array < 2 then
+			-- array only has less than 2 items, no sorting needed
+			return array
+		end
+		return algorithms.dir[algoNo](array, sortingargs)
+	end
+end
+
+function algorithms.bubblesort(array, _)
+	for x = 1, #array do
+		for y = 1 #array -x do
+			if array[y] > array[y +1]:
+				local c = array[y]
+				array[y] = array[y +1]
+				array[y +1] = c
+			end
+		end
+	end
+	return array
+end
+
+function algorithms.insertionsort(array, _)
+	for x = 2, #array do
+		y = x -1
+		while y > 0 and array[y] < array[x] do
+			array[y +1] = array[y]
+			y = y - 1
+		end
+		array[y +1] = array[x]
+	end
+	return array
+end
+
+local function __heapify(array, n, i)
+	local largest = i
+	local l = 2 *i
+	local r = 2 *i +1
+
+	if l <= n and array[l] > array[largest] then
+		-- right child node exists
+		largest = l
+	end
+	if r <= n and array[r] > array[largest] then
+		-- left child node exists
+		largest = r
+	end
+
+	if largest ~= i then
+		array[i], array[largest] = array[largest], array[i]
+		__heapify(array, n, largest)
+	end
+end
+function algorithms.heapsort(array)
+	n = #array
+
+	for i = n//2, 1, -1 do
+		__heapify(array, n, i)
+	end
+
+	for i = n, 2, -1 do
+		array[i], array[1] = array[1], array[i]
+		__heapify(array, n, 1)
+	end
+	return array
+end
+
+local function __getpartition(l, h, array, pivot)
+	local startingpivot = l --round(#array/2 -.5)
+	if pivot ~= nil then
+		startingpivot = pivot
+	end
+
+	local pivotvalue = array[startingpivot]
+	local i = l
+	local j = h
+	while i < j do
+		while array[i] <= pivotvalue do
+			i = i + 1 -- i++ ;(
+		end
+		while array[j] > pivotvalue do
+			j = j - 1
+		end
+
+		if (i < j) then
+			array[i], array[j] = array[j], array[i]
+		end
+	end
+
+	array[startingpivot], array[j] = array[j], array[startingpivot]
+	return startingpivot, j
+end
+local function __quicksort(l, h, array, startingpivot)
+	-- recursive
+	if l < h then
+		local initial, j = __getpartition(l, h, array, startingpivot)
+		if not (initial == j) then
+			__quicksort(l, j, array)
+			__quicksort(j +1, h, array)
+		else
+			-- sorted
+			--print("sorted already")
+		end
+	else
+		-- print("sorted by < 2 values")
+		-- array has less than 2 values (1)
+		-- no need to return, since array is being modified
+	end
+end
+function algorithms.quicksort(array, args)
+	local startingpivot = round(#array/2 -.5)
+	if args ~= nil and args.Pivot ~= nil and type(args.Pivot) == "number" and args.Pivot > 0 and args.Pivot <= #array then
+		startingpivot = args.Pivot
+	elseif args ~= nil and args.Pivot ~= nil then
+		-- failed to use args.Pivot
+		warn(".Pivot argument in SorterObject.SortingParameters was invalid; did not use")
+	end
+
+	__quicksort(1, #array, array, startingpivot)
+	return array
+end
+
+return algorithms
---
+++ main.lua
@@ -0,0 +1,213 @@
+--!strict
+
+module_container = {}
+alogrithm = require(script.algorithms)
+
+mainsorterclass = {}
+mainsorterclass.__index = mainsorterclass
+
+-- types
+export type SorterObject = {
+	Ascending: boolean,
+	Algorithm: number,
+	IncludeNonSorted: boolean,
+	SortFirstFew: boolean,
+	SortingParameters: type SortingParameterObject = {
+		Pivot: number
+	}
+}
+
+local function __FillSorterObjectParams(): SorterObject
+	return {
+		Ascending = true,
+		Algorithm = 1,
+		IncludeNonSorted = true,
+		SortFirstFew = -1,
+		SortingParameters = {}
+	}
+end
+
+local function __type_check(var, insisted_type: string)
+	return type(var) == insisted_type
+end
+local function __inrange_check(var, min: number, max: number, inclusive: string)
+	-- min must always be lower than max for it to logical work
+	-- inclusive = "00" for both non inclusive
+	-- inclusive = "01" for max value inclusive, min value non inclusive
+	if inclusive == nil then
+		inclusive = "00"
+	end
+
+	local lowerbound, upperbound = false, false
+	if inclusive:sub(1, 1) == "0" then
+		-- non inclusive for min
+		lowerbound = var > min
+	else
+		lowerbound = var >= min
+	end
+	if inclusive:sub(2, 2) == "0" then
+		-- non inclusive for max
+		upperbound = var < max
+	else
+		upperbound = var <= max
+	end
+	return lowerbound and upperbound
+end
+
+
+function module_container.new(properties)
+	local x: SorterObject = __FillSorterObjectParams()
+	setmetatable(x, mainsorterclass)
+
+	x:OverwriteProperties(properties)
+	return x
+end
+
+function mainsorterclass:OverwriteProperties(properties)
+	if properties.Ascending then
+		if not __type_check(properties.Ascending, "boolean") then
+			warn(".Ascending value must be a boolean")
+		else
+			self.Ascending = properties.Ascending
+		end
+	end
+	if properties.Algorithm then
+		if not __type_check(properties.Algorithm, "number") then
+			warn(".Algorithm value must be a number")
+		elseif not __inrange_check(properties.Algorithm, 0, algorithms.LENGTH, "11") then
+			warn(".Algorithm value must be within 1 - %d(inclusive)":format(algorithms.LENGTH))
+		else
+			self.Algorithm = properties.Algorithm
+		end
+	end
+	if properties.IncludeNonSorted then
+		if not __type_check(properties.IncludeNonSorted, "boolean") then
+			warn(".IncludeNonSorted value must be a boolean")
+		else
+			self.IncludeNonSorted = properties.IncludeNonSorted
+		end
+	end
+	if properties.SortFirstFew then
+		if not __type_check(properties.SortFirstFew, "number") then
+			warn(".SortFirstFew must be a number")
+		else
+			self.SortFirstFew = properties.SortFirstFew
+		end
+	end
+end
+
+function mainsorterclass:Flip(array)
+	-- flips/reverse the array
+	-- i.e, array = {3, 1, 5, 10}, return = {10, 5, 1, 3}
+	-- NOTE: Modifies the actual array passed as an argument
+	for i = 1, #array //2 do
+		array[i], array[#array -i +1] = array[#array -i +1], array[i]
+	end
+	return array
+end
+
+function mainsorterclass:Slice(array, s: number, e: number, step: number)
+	-- s = start, must be positive
+	-- e = end, must be positive
+	-- step = step value, must be positive
+	-- will slice beginning from the start(inclusive) and ending before end value
+	-- will return a new array, leaving the passed array(argument) un-modified
+	if (s == nil or type(s) ~= "number") then
+		error("Start value must be a number")
+	elseif (e == nil or type(e) ~= "number") then
+		error("End value must be a number")
+	elseif (step == nil or type(step) ~= "number") then
+		error("Step value must be a number")
+	elseif s <= 0 or e <= 0 or step <= 0 then
+		error("Start, end, step values must all be positive (> 0)")
+	end
+
+	local re = {}
+	for i = s, e -1, step do
+		table.insert(re, array[i])
+	end
+	return re
+end
+
+function mainsorterclass:Concat(a, b, modify_array)
+	-- concatenate two arrays, a and b
+	-- i.e, a = {3, 1}, b = {3, 5}, return = {3, 1, 3, 5}
+	-- if modify_array is false, it will create a new table
+	if modify_array == nil then
+		modify_array = true -- default value
+	end
+
+	local new = {}
+	if modify_array then
+		new = a
+	else
+		for i = 1, #a do
+			table.insert(new, a[i])
+		end
+	end
+	for i = 1, #b do
+		table.insert(new, b[i])
+	end
+	return new
+end
+
+function mainsorterclass:Randomise(array, seed)
+	-- randomises the array
+end
+
+function mainsorterclass:Sort(array)
+	local ToSort = {}
+	local ToAppend = {} -- to store the non numerical values
+
+	local iteration_limit
+	if self.SortFirstFew > 0 then
+		iteration_limit = self.SortFirstFew //1
+	end
+
+	for _, v in pairs(array) do
+		if type(v) == "number" and (iteration_limit == nil or iteration_limit > 0) then
+			table.insert(ToSort, v)
+		else
+			table.insert(ToAppend, v)
+		end
+		if iteration_limit ~= nil then
+			iteration_limit = iteration_limit - 1
+		end
+	end
+
+	local SortedList = algorithm.distributor(self.Algorithm, ToSort, self.SortingParameters)
+	if not self.Ascending then
+		-- SortedList is in Ascending
+		SortedList = self:Flip(SortedList)
+	end
+	if self.IncludeNonSorted then
+		-- Appends ToAppend to SortedList
+		ToSort = self:Concat(ToSort, ToAppend, true)
+	end
+	return ToSort
+end
+
+function mainsorterclass:__recurse_array(array)
+	local ToSort = {}
+	local ToAppend = {}
+
+	for _, v in pairs(array) do
+		if type(v) == "table" then
+			self:__recurse_array(v)
+		elseif type(v) ~= "number" then
+			table.insert(ToAppend, v)
+		else
+			table.insert(ToSort, v)
+		end
+	end
+end
+
+
+function mainsorterclass:DeepSort(array)
+	-- takes in an array where there are nested arrays
+	-- i.e, array = {3, 1, 5, 10, {1, 3, 10, 5, 4, {4, 3, 1, 2}}}
+	-- return = {1, 3, 5, 10, {1, 3, 4, 5, 10, {1, 2, 3, 4}}}
+	local master = {} -- stores all the results
+end
+
+return module_container
